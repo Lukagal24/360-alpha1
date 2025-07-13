af3dc0ed727a8e60ee4c13b494253f9b0f9738fd
Lukagal24 <galc.novak@gmail.com>
2025-07-13 18:40:02 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,193 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20250703_113941_528",
+      "name": "20250703_113941_528",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.24264655530727808,
+        "pitch": 0.5217785868655831,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.12206021021740909,
+          "pitch": 0.9265710106494307,
+          "title": "Trimmer",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.772504527094803,
+          "pitch": -0.3327614222618607,
+          "title": "Ročica za sprožitev BPRS",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5099898778804626,
+          "pitch": 0.7226636999206679,
+          "title": "Krmilna palica",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.23553872216417204,
+          "pitch": 0.7965397661720868,
+          "title": "Čok motorja",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6138180196971739,
+          "pitch": 0.5772230281769666,
+          "title": "Krmilo (rudder)",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.22727353078225931,
+          "pitch": 0.2102492148649855,
+          "title": "Navidezni horizont",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.24775343165926955,
+          "pitch": 0.13769219137020627,
+          "title": "Žiro kompas",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8339162207515223,
+          "pitch": 1.0651840351405006,
+          "title": "Ročica za flape",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2050058521690481,
+          "pitch": 0.5083239880832586,
+          "title": "Koordinator zavoja",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.03625266066725352,
+          "pitch": 0.20832347007772967,
+          "title": "ELT stikalo",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4839216187381812,
+          "pitch": 0.2011437304385133,
+          "title": "Indikator hitrost",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2676664852719739,
+          "pitch": 0.425596858587614,
+          "title": "Radio &amp; transponder",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.05794329105037832,
+          "pitch": 1.0009948427695718,
+          "title": "Zavore &amp;&nbsp;<div>parkirna zavora</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.05931641170119484,
+          "pitch": 0.8125736392261587,
+          "title": "Ročica za moč motorja",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5132982610262413,
+          "pitch": 0.39784240929486714,
+          "title": "Trim indikator",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.12616917379657622,
+          "pitch": 0.7159683828826999,
+          "title": "Varovalke",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.14211413871608514,
+          "pitch": 0.658039611060568,
+          "title": "Stikala za luči",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.33715211299134573,
+          "pitch": 0.6348147845321996,
+          "title": "Master &amp; avionics stikala",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6328328841901367,
+          "pitch": 0.37381571102256217,
+          "title": "Selektor za zračne zavore",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.052669510248730234,
+          "pitch": 0.30529269317609575,
+          "title": "Zaslon s parametri motorja",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2586673579866776,
+          "pitch": 0.3125303168209399,
+          "title": "Zaslon za GPS",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.13267002024635488,
+          "pitch": 0.3024955895510253,
+          "title": "Indikator vertikalne hitrosti",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4418860580224617,
+          "pitch": 0.37343526687973494,
+          "title": "Indikator za RPM in motorske ure",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.13073988725124508,
+          "pitch": 0.17954526541068283,
+          "title": "Indikator višine",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.05040931564068174,
+          "pitch": 0.6774551748934421,
+          "title": "Ročica za nastavitev oddaljenosti krmila",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
